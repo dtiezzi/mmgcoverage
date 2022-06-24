03d9c2553f597ce18984d2caa31d5aa4a4734143
dtiezzi <dtiezzi@usp.br>
2022-06-23 21:24:24 -0300

    dtiezzi editing scripts

--- app.py
+++ app.py
@@ -11,27 +11,18 @@ app = Flask(__name__)
 app.static_folder = 'static'
 
 uf_dic = UfBr().state_dict
-LoadFiles(['_geojson/brstates.geojson', '_geojson/br_mun_all.json']).loadjson()
-LoadFiles(['csv/Supplemental_table_states_data.csv', 'csv/Supplemental_table_municipality_data.csv']).loadcsv()
+states, counties = LoadFiles(['_geojson/brstates.geojson', '_geojson/br_mun_all.json']).loadjson()
+st_df, ct_df = LoadFiles(['csv/Supplemental_table_states_data.csv', 'csv/Supplemental_table_municipality_data.csv']).loadcsv()
 
-geofileST = '_geojson/brstates.geojson'
-geofileCT = '_geojson/br_mun_all.json'
-
-DATA_STATES = "csv/Supplemental_table_states_data.csv"
-DATA_MUNICIP = "csv/Supplemental_table_municipality_data.csv"
-st_df = pd.read_csv(DATA_STATES, index_col=False)
+## Filt columns
 st_df = st_df[['ibge_uf_id','uf', 'avg_mmg_coverage_50_69_ans_adj','avg_mmg_coverage_40_49_ans_adj','ebc_det_ratio_50_69','ebc_det_ratio_40_49', 'avg_population']]
-st_json = open(geofileST)
-states = json.load(st_json)
-ct_df = pd.read_csv(DATA_MUNICIP, index_col=False)
 ct_df = ct_df[['uf', 'name_uf', 'ibge_munic_id', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'mmg_coverage_40_49_ans_adj_max100', 'ebc_det_ratio_50_69', 'ebc_det_ratio_40_49', 'total_number_mmg_50_69', 'total_number_mmg_40_49', 'avg_population']]
-ct_json = open(geofileCT)
-counties = json.load(ct_json)
+
 
 
 uf_unique_df = ct_df[ct_df['uf'] == 'SP']
 uf = uf_dic['SP']
-uniq_json_file = '_geojson/munic/geojs-{}-mun.json'.format(uf)
+uniq_json_file = f'_geojson/munic/geojs-{uf}-mun.json'
 uniq_json = open(uniq_json_file)
 uniq_municipalities = json.load(uniq_json)   
 lon, lat = latlon(uniq_municipalities)
--- modules/loadfiles.py
+++ modules/loadfiles.py
@@ -4,10 +4,17 @@ import json
 class LoadFiles():
 
     def __init__(self, *args) -> None:
-        self.files = args
+        self.files = args[0]
 
     def loadjson(self):
-        pass
+        json_files = []
+        for file in [f for f in self.files if f.endswith('json')]:
+            jsonfile = open(file)
+            json_files.append(json.load(jsonfile))
+        return json_files
 
     def loadcsv(self):
-        pass
+        csv_files = []
+        for file in [f for f in self.files if f.endswith('.csv')]:
+            csv_files.append(pd.read_csv(file, index_col=False))
+        return csv_files
