b81fa16d7bf6b70ad21650968e0b04c5bd25edff
dtiezzi <dtiezzi@usp.br>
2022-06-27 18:27:43 -0300

    dtiezzi maps OK, sidebar OK

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request
 from modules.brmaps import *
 from modules.ufIbge import UfBr
 from modules.loadfiles import LoadFiles
@@ -11,43 +11,61 @@ app = Flask(__name__)
 app.static_folder = 'static'
 
 uf_dic = UfBr().state_dict
-states, counties = LoadFiles(['/var/www/mmgcov.com/_geojson/brstates.geojson', '/var/www/mmgcov.com/_geojson/br_mun_all.json']).loadjson()
-st_df, ct_df = LoadFiles(['/var/www/mmgcov.com/csv/Supplemental_table_states_data.csv', '/var/www/mmgcov.com/csv/Supplemental_table_municipality_data.csv']).loadcsv()
+uf_names_dic = UfBr().state_name_dict
+uf_area_dic = UfBr().state_area_dict
+uf_area_dic = {k: 1 - (uf_area_dic[k] - min(uf_area_dic.values())) / (max(uf_area_dic.values()) - min(uf_area_dic.values())) for k in uf_area_dic}
+print(uf_area_dic)
+
+states, counties = LoadFiles(['_geojson/brstates.geojson', '_geojson/br_mun_all.json']).loadjson()
+st_df, ct_df = LoadFiles(['csv/Supplemental_table_states_data.csv', 'csv/Supplemental_table_municipality_data.csv']).loadcsv()
 
 ## Filt columns
 st_df = st_df[['ibge_uf_id','uf', 'avg_mmg_coverage_50_69_ans_adj','avg_mmg_coverage_40_49_ans_adj','ebc_det_ratio_50_69','ebc_det_ratio_40_49', 'avg_population']]
 ct_df = ct_df[['uf', 'name_uf', 'ibge_munic_id', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'mmg_coverage_40_49_ans_adj_max100', 'ebc_det_ratio_50_69', 'ebc_det_ratio_40_49', 'total_number_mmg_50_69', 'total_number_mmg_40_49', 'avg_population']]
 
-
-
-uf_unique_df = ct_df[ct_df['uf'] == 'SP']
-uf = uf_dic['SP']
-uniq_json_file = f'/var/www/mmgcov.com/_geojson/munic/geojs-{uf}-mun.json'
-uniq_json = open(uniq_json_file, "rb")
-uniq_municipalities = json.load(uniq_json)   
-lon, lat = latlon(uniq_municipalities)
-
-fig1 = plotmapCounties(uf_unique_df, uniq_municipalities, ctitle='Mamográfica Média de 2010-2019 (50-70 anos)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'], zoom=4, lat=lat, lon=lon)
-div = fig1.to_html(full_html=False)
-
-figBr1 = plotmapCounties(ct_df, counties, ctitle='Mamográfica Média de 2010-2019 (50-70 anos)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'])
-
-figBr2 = plotmapCounties(ct_df, counties, ctitle='Mamográfica Média de 2010-2019 (40-50 anos)', colname='mmg_coverage_40_49_ans_adj_max100', chover=['uf', 'mmg_coverage_40_49_ans_adj_max100', 'ebc_det_ratio_40_49'], age=40)
+figBr1 = plotmapCounties(ct_df, counties, ctitle='Mean mammogram coverage from 2010 to 2019 (50-70 years)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'])
+figBr2 = plotmapCounties(ct_df, counties, ctitle='Mean mammogram coverage from 2010 to 2019 (40-50 years)', colname='mmg_coverage_40_49_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_40_49_ans_adj_max100', 'ebc_det_ratio_40_49'], age=40)
 divBr1 = figBr1.to_html(full_html=False)
 divBr2 = figBr2.to_html(full_html=False)
 
+
+
 @app.route('/')
 def index():
-    return render_template('/index.html', figure = div)
+    return render_template('/index.html', figure = [divBr1, divBr2])
 
-@app.route('/brmaps')
+@app.route('/brstmaps')
 def brmaps():
-    return render_template('/brmaps.html', figure = [divBr1, divBr2])
+    figBrSt1 = plotmapStates(st_df, states, ctitle='Mean mammogram coverage from 2010 to 2019 (50-0 years)', colname='avg_mmg_coverage_50_69_ans_adj', chover=['uf', 'avg_mmg_coverage_50_69_ans_adj', 'ebc_det_ratio_50_69'])
+    figBrSt2 = plotmapStates(st_df, states, ctitle='Mean mammogram coverage from 2010 to 2019 (40-50 years)', colname='avg_mmg_coverage_40_49_ans_adj', chover=['uf', 'avg_mmg_coverage_50_69_ans_adj', 'ebc_det_ratio_40_49'])
+    divBrSt1 = figBrSt1.to_html(full_html=False)
+    divBrSt2 = figBrSt2.to_html(full_html=False)
+    return render_template('/brstmaps.html', figure = [divBrSt1, divBrSt2])
+
+@app.route('/stmaps', methods=['GET', 'POST'])
+def stmaps():
+    if request.method == 'POST':
+        uf = request.form.get('state')
+    else:
+        uf = 'SP'
+    uf_unique_df = ct_df[ct_df['uf'] == uf]
+    uf_cod = uf_dic[uf]
+    uf_name = uf_names_dic[uf]
+    uf_area = uf_area_dic[uf]
+    uniq_json_file = f'_geojson/munic/geojs-{uf_cod}-mun.json'
+    uniq_json = open(uniq_json_file, "rb")
+    uniq_municipalities = json.load(uniq_json)   
+    lon, lat = latlon(uniq_municipalities)
+    fig1 = plotmapCounties(uf_unique_df, uniq_municipalities, ctitle='Mean mammogram coverage from 2010 to 2019 (50-70 years)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'], zoom=4 + uf_area, lat=lat, lon=lon)
+    fig2 = plotmapCounties(uf_unique_df, uniq_municipalities, ctitle='Mean mammogram coverage from 2010 to 2019 (40-50 years)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'], zoom=4 + uf_area, lat=lat, lon=lon)
+    div1 = fig1.to_html(full_html=False)
+    div2 = fig2.to_html(full_html=False)
+    return render_template('/stmaps.html', figure = [div1, div2], uf_name = uf_name)
 
 @app.route('/aboutProject')
 def about():
     return render_template('/aboutProject.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=6747)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=6747)
 
--- init_test.py
+++ init_test.py
@@ -1,10 +1,70 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from modules.brmaps import *
+from modules.ufIbge import UfBr
+from modules.loadfiles import LoadFiles
+import pandas as pd
+import json
+import os
 
 app = Flask(__name__)
 
+app.static_folder = 'static'
+
+uf_dic = UfBr().state_dict
+uf_names_dic = UfBr().state_name_dict
+uf_area_dic = UfBr().state_area_dict
+uf_area_dic = {k: 1 - (uf_area_dic[k] - min(uf_area_dic.values())) / (max(uf_area_dic.values()) - min(uf_area_dic.values())) for k in uf_area_dic}
+print(uf_area_dic)
+
+states, counties = LoadFiles(['_geojson/brstates.geojson', '_geojson/br_mun_all.json']).loadjson()
+st_df, ct_df = LoadFiles(['csv/Supplemental_table_states_data.csv', 'csv/Supplemental_table_municipality_data.csv']).loadcsv()
+
+## Filt columns
+st_df = st_df[['ibge_uf_id','uf', 'avg_mmg_coverage_50_69_ans_adj','avg_mmg_coverage_40_49_ans_adj','ebc_det_ratio_50_69','ebc_det_ratio_40_49', 'avg_population']]
+ct_df = ct_df[['uf', 'name_uf', 'ibge_munic_id', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'mmg_coverage_40_49_ans_adj_max100', 'ebc_det_ratio_50_69', 'ebc_det_ratio_40_49', 'total_number_mmg_50_69', 'total_number_mmg_40_49', 'avg_population']]
+
+figBr1 = plotmapCounties(ct_df, counties, ctitle='Mean mammogram coverage from 2010 to 2019 (50-70 years)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'])
+figBr2 = plotmapCounties(ct_df, counties, ctitle='Mean mammogram coverage from 2010 to 2019 (40-50 years)', colname='mmg_coverage_40_49_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_40_49_ans_adj_max100', 'ebc_det_ratio_40_49'], age=40)
+divBr1 = figBr1.to_html(full_html=False)
+divBr2 = figBr2.to_html(full_html=False)
+
+
+
 @app.route('/')
 def index():
-    return 'Hello from Flask'
+    return render_template('/index.html', figure = [divBr1, divBr2])
 
-#if __name__ == '__main__':
-#    app.run()
+@app.route('/brstmaps')
+def brmaps():
+    figBrSt1 = plotmapStates(st_df, states, ctitle='Mean mammogram coverage from 2010 to 2019 (50-0 years)', colname='avg_mmg_coverage_50_69_ans_adj', chover=['uf', 'avg_mmg_coverage_50_69_ans_adj', 'ebc_det_ratio_50_69'])
+    figBrSt2 = plotmapStates(st_df, states, ctitle='Mean mammogram coverage from 2010 to 2019 (40-50 years)', colname='avg_mmg_coverage_40_49_ans_adj', chover=['uf', 'avg_mmg_coverage_50_69_ans_adj', 'ebc_det_ratio_40_49'])
+    divBrSt1 = figBrSt1.to_html(full_html=False)
+    divBrSt2 = figBrSt2.to_html(full_html=False)
+    return render_template('/brstmaps.html', figure = [divBrSt1, divBrSt2])
+
+@app.route('/stmaps', methods=['GET', 'POST'])
+def stmaps():
+    if request.method == 'POST':
+        uf = request.form.get('state')
+    else:
+        uf = 'SP'
+    uf_unique_df = ct_df[ct_df['uf'] == uf]
+    uf_cod = uf_dic[uf]
+    uf_name = uf_names_dic[uf]
+    uf_area = uf_area_dic[uf]
+    uniq_json_file = f'_geojson/munic/geojs-{uf_cod}-mun.json'
+    uniq_json = open(uniq_json_file, "rb")
+    uniq_municipalities = json.load(uniq_json)   
+    lon, lat = latlon(uniq_municipalities)
+    fig1 = plotmapCounties(uf_unique_df, uniq_municipalities, ctitle='Mean mammogram coverage from 2010 to 2019 (50-70 years)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'], zoom=4 + uf_area, lat=lat, lon=lon)
+    fig2 = plotmapCounties(uf_unique_df, uniq_municipalities, ctitle='Mean mammogram coverage from 2010 to 2019 (40-50 years)', colname='mmg_coverage_50_69_ans_adj_max100', chover=['uf', 'name_munic', 'mmg_coverage_50_69_ans_adj_max100', 'ebc_det_ratio_50_69'], zoom=4 + uf_area, lat=lat, lon=lon)
+    div1 = fig1.to_html(full_html=False)
+    div2 = fig2.to_html(full_html=False)
+    return render_template('/stmaps.html', figure = [div1, div2], uf_name = uf_name)
+
+@app.route('/aboutProject')
+def about():
+    return render_template('/aboutProject.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
